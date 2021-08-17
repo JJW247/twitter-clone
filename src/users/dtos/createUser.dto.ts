0a73e2c4ef34cf8a4ec023829c9951f16019e80d
h662 <h662hong@gmail.com>
2021-08-17 15:49:59 +0900

    IsNotEmpty

--- src/users/dtos/createUser.dto.ts
+++ src/users/dtos/createUser.dto.ts
@@ -1,13 +1,16 @@
-import { IsEmail, IsString, Length } from 'class-validator';
+import { IsEmail, IsNotEmpty, IsString, Length } from 'class-validator';
 
 export class CreateUserDto {
   @IsEmail()
+  @IsNotEmpty()
   email: string;
 
   @IsString()
   @Length(2, 10)
+  @IsNotEmpty()
   nickname: string;
 
   @IsString()
+  @IsNotEmpty()
   password: string;
 }
